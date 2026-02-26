08e40e06c6088ad45284ed3fe711b167221b86b8
ytrqua3 <64173212+ytrqua3@users.noreply.github.com>
2026-02-26 04:05:32 -0800

    Add files via upload

--- api_handler.py
+++ api_handler.py
@@ -8,14 +8,30 @@ def lambda_handler(event, context):
 
     print("✅Successfully retreived user data from http request")
     print(body)
-    ENDPOINT_NAME = "music-preference-v20"
+    if ("top_artists" in body) and ("total_scrobbles" in body):
+        if len(body["top_artists"]) != 50:
+            return {
+                "statusCode": 400,
+                "body": json.dumps({
+                    "error": "top_artists must be of length 50"
+                })
+            }
+    else:
+        return {
+            "statusCode": 400,
+            "body": json.dumps({
+                "error": "Missing required fields"
+            })
+        }
 
+    ENDPOINT_NAME = "music-preference-v25"
+    body_bytes = json.dumps(body).encode('utf-8')
     runtime = boto3.client("sagemaker-runtime")
     response = runtime.invoke_endpoint(
             EndpointName=ENDPOINT_NAME,
             ContentType="application/json",
             Accept="application/json",          
-            Body=body
+            Body=body_bytes
         )
 
     result = json.loads(response["Body"].read().decode('utf-8'))
